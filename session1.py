811df88b0044c03621750d875683a53faf975597
avnigoel98 <avni.goel@insight.com>
2024-05-26 00:35:01 +0530

    first_commit

---
+++ session1.py
@@ -0,0 +1,33 @@
+
+
+# print(3+9)
+'''print("hello")
+dfghjkl
+dfghjkl
+sdfghjkl
+dfghjkhgfdsaghjkL;KJHGFCV
+print(6.9 < 5)'''
+
+# num_1 = 5
+# num_2 = 4
+# sum = num_1+num_2
+
+# print(sum)
+
+
+
+# print(5/2) # divide
+# print(5%2) # remainder  (% - modulus)
+
+
+# print(6==5) 
+
+# print(5 != 5)
+# num = 0
+# print(num)
+
+
+num = 4
+print(num)
+num += 2   # num = num + 2
+print(num)
