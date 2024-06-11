a46a40a0472269017f8fee2be4b66b13be3aa806
avnigoel98 <avni.goel@insight.com>
2024-06-11 23:02:01 +0530

    working on if/else

---
+++ session5.py
@@ -0,0 +1,73 @@
+# Conditional Statements
+# if
+# elif (else if)
+# else
+
+# a = 120
+
+# if (a > 90):
+#     print("A grader")
+# elif(a > 80):
+#     print("B grader")
+# elif(a > 40):
+#     print("average")
+# else:
+#     print("fail")
+
+# if(a > 90):
+#     print("hello")
+# #----------------
+# if(a > 80):
+#     print("bye")
+# #----------------
+
+# if(a > 12):
+#     print("abcd")
+# else:
+#     print("1234")
+# #----------------
+
+
+# age = int(input("Enter your age: "))
+
+# if(age > 18):
+#     print("You can vote")
+# else:
+#     print("Can't vote")
+
+# a = 45
+
+# if (a > 90  and a < 100):
+#     print("A grader")
+# elif(a > 80 and a < 90):
+#     print("B grader")
+# elif(a > 40):
+#     print("average")
+# else:
+#     print("fail")
+
+# write a program to find out whether a student is pass or fail
+# if it requires total 40% and at least 33 in each subject to pass
+# Assume 3 subjects and take marks as an input from the user
+
+
+# is
+# in
+
+# a = None
+
+# if(a is None):
+#     print("yes")
+# else:
+#     print("no")
+
+# a = [45, 78, 90]
+# print(45 in a)
+
+# A spam comment is defined as a text containing following keywords:
+# ['buy now' , 'subscribe this', 'click here', 'make a lot of money']
+
+# write a prog to detect these spams
+# take a user input for a message or a mail
+
+# Write a program to find greatest of four numbers entered by the user
